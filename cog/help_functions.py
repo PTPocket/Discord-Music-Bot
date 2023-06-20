95d775763d7d5c34418c38d510fd0bb613ed280d
PetiePocket <pocketgeneral@outlook.com>
2023-06-19 19:08:46 -0700

    fixed last track printing

--- cog/help_functions.py
+++ cog/help_functions.py
@@ -145,16 +145,17 @@ class MusicFunctions(View):
 
             if self.data.get_history(guild_id) == []:
                 return
-            if len(self.data.get_history(guild_id)) == 1:
-                recent = self.data.data[guild_id]['history'].pop(0)
-                self.data.prepend_to_queue(guild_id, recent)
-            else:
-                recent = self.data.data[guild_id]['history'].pop(0)
-                self.data.prepend_to_queue(guild_id, recent)
-                recent = self.data.data[guild_id]['history'].pop(0)
-                self.data.prepend_to_queue(guild_id, recent)
+
 
             if bot_voice.is_playing() or bot_voice.is_paused():
+                if len(self.data.get_history(guild_id)) == 1:
+                    recent = self.data.data[guild_id]['history'].pop(0)
+                    self.data.prepend_to_queue(guild_id, recent)
+                else:
+                    recent = self.data.data[guild_id]['history'].pop(0)
+                    self.data.prepend_to_queue(guild_id, recent)
+                    recent = self.data.data[guild_id]['history'].pop(0)
+                    self.data.prepend_to_queue(guild_id, recent)
                 send_log(guild_name, 'LAST TRACK')
                 self.data.set_loop(guild_id, False)
                 self.music_cog.to_print.append(guild_id)
@@ -162,7 +163,10 @@ class MusicFunctions(View):
                 await interaction.response.defer()
                 await print_music_player(self.music_cog, guild_id, self.data)
             else:
+                recent = self.data.data[guild_id]['history'].pop(0)
+                self.data.prepend_to_queue(guild_id, recent)
                 await voice_connect(interaction)
+                self.music_cog.to_print.append(guild_id)
                 self.music_cog.music_player_start(interaction)
                 await interaction.response.defer()
                 await print_music_player(self.music_cog, guild_id, self.data)
