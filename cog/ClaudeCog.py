73139f8c6f42dcfbd5921ff5559d03aa4586adab
PetiePocket <pocketgeneral@outlook.com>
2024-12-10 14:25:40 -0800

    claude now resets text channel after 3-6 min of inactivity

--- cog/ClaudeCog.py
+++ cog/ClaudeCog.py
@@ -11,60 +11,92 @@ SYSTEM_CHARACTERISTICS = 'You are a helpful assistant named PocBot created by Po
 MAX_TOKENS = 1000
 TEMPERATURE = 1
 
+class ChannelInformation():
+    def __init__(self):
+        self.channels = {}
+        self.prev_msg_timestamp = {}
+        self.msg_history = {}
+
+    def GetTimestamps(self):
+        return self.prev_msg_timestamp.copy()
+
+    def GetChannel(self, channel_id):
+        return self.channels.get(channel_id)
+
+    def GetMsgHistory(self, channel_id):
+        return self.msg_history.get(channel_id)
+
+    def SaveUserPrompt(self, channel:discord.channel.TextChannel, prompt:str):
+        channel_id = channel.id
+
+        if channel_id not in self.msg_history.keys():
+            self.msg_history[channel_id] = []
+
+        self.channels[channel_id] = channel
+        self.msg_history[channel_id].append(
+            {"role": "user", "content": prompt}
+        )
+        self.prev_msg_timestamp[channel_id] = datetime.now()
+        return
+
+    def SaveAssistantPrompt(self, channel:discord.channel.TextChannel, prompt:str):
+        channel_id = channel.id
+        self.msg_history[channel_id].append({"role": "assistant", "content": prompt})
+
+    def RemoveChannel(self, channel_id):
+        self.channels.pop(channel_id)
+        self.prev_msg_timestamp.pop(channel_id)
+        self.msg_history.pop(channel_id)
+
 class ClaudeCog(commands.Cog):
     def __init__(self, bot:commands.Bot):
         self.bot = bot
         self.client = anthropic.Anthropic()
+        self.channel_info = ChannelInformation()
+        self.PurgeChannels.start()
 
-        self.last_message_time = {}
-        self.user_message_history = {}
-
-            
     @commands.Cog.listener() 
     async def on_message(self, message:discord.message.Message):
-        user_id = message.author.id
-        if self.bot.user.id == user_id:
+        try:
+            if self.bot.user.id == message.author.id:
+                return
+            if type(message.channel) == discord.channel.DMChannel:
+                response = await self.ClaudeResponse(message.channel, message.content)
+                await message.channel.send(response)
+            elif message.channel.name == 'pocbot-ama':
+                response = await self.ClaudeResponse(message.channel, message.content)
+                await message.channel.send(response)
             return
-        if type(message.channel) == discord.channel.DMChannel:
-            response = await self.ClaudeResponse(user_id, message.content)
-            await message.channel.send(response)
-        elif message.channel.name == 'pocbot-ama':
-            response = await self.ClaudeResponse(user_id, message.content)
-            await message.channel.send(response)
-        return
+        except Exception as e:
+            print(e)
 
-
-    async def ClaudeResponse(self, user_id, prompt):
-        self.CheckTime(user_id)
-        self.RecordMessage(user_id, prompt)
+    async def ClaudeResponse(self,  channel:discord.channel.TextChannel, prompt:str):
+        self.channel_info.SaveUserPrompt(channel, prompt)
         output = self.client.messages.create(
             model       = MODEL_HAIKU,
             system      = SYSTEM_CHARACTERISTICS,
             max_tokens  = MAX_TOKENS,
             temperature = TEMPERATURE,
-            messages    = self.user_message_history[user_id]
+            messages    = self.channel_info.GetMsgHistory(channel.id)
         )
         claude_output = output.content[0].text
-
-        self.RecordMessage(user_id, claude_output, assistant=True)
+        self.channel_info.SaveAssistantPrompt(channel, claude_output)
         return claude_output
     
-    def RecordMessage(self, user_id, prompt, assistant = False):
-        if user_id not in self.user_message_history.keys():
-            self.user_message_history[user_id] = []
-        if assistant is False:
-            self.user_message_history[user_id].append({"role": "user", "content": prompt})
-            self.last_message_time[user_id] = datetime.today()
-        else:
-            self.user_message_history[user_id].append({"role": "assistant", "content": prompt})  
-
-    def CheckTime(self, user_id):
-        if user_id not in self.last_message_time:
-            return
-        prev_time = self.last_message_time[user_id]
+    @tasks.loop(seconds=180)
+    async def PurgeChannels(self):
+        timestamps = self.channel_info.GetTimestamps()
         curr_time = datetime.today()
-        diff_time = curr_time-prev_time
-        if diff_time.seconds > 180:
-            self.last_message_time.pop(user_id)
-            self.user_message_history.pop(user_id)
-        
+        to_delete = []
+        for channel_id in timestamps:
+            prev_time = timestamps.get(channel_id)
+            diff_time = (curr_time-prev_time).seconds
+            if diff_time > 180:
+                to_delete.append(channel_id)
+                channel = self.channel_info.GetChannel(channel_id)
+                self.channel_info.RemoveChannel(channel_id)
+                if type(channel) != discord.channel.DMChannel:
+                    await channel.purge()
+                    await channel.send("Hi! Ask me anything.")
+                    
+                    
