3f08d7ad4382189980c2c00b48bf16a83ae125c8
PetiePocket <pocketgeneral@outlook.com>
2025-07-27 00:54:18 -0700

    update path link to files folder

--- GlobVar.py
+++ GlobVar.py
@@ -1,10 +1,10 @@
 
 import os
 
-MAIN_PATH        = f'C:\\Users\\Main Server\\Documents\\Music Bot Files'
-FFMPEG_EXE_PATH  = MAIN_PATH + '\\ffmpeg\\ffmpeg.exe'
-LOCAL_MUSIC_PATH = MAIN_PATH + '\\music\\Formatted'
-SETTING_PATH     = MAIN_PATH + '\\DB\\Music Bot Setting.json'
-LOG_FOLDER_PATH  = MAIN_PATH + '\\DB\\Music Bot Log Folder'
-EXP_FILE_PATH    = MAIN_PATH + '\\DB\\Exp Data.json'
-INVALID_URL_PATH = MAIN_PATH + '\\DB\\invalidUrls.txt'
+MAIN_PATH        = f'Music Bot Files'
+FFMPEG_EXE_PATH  = MAIN_PATH + '/ffmpeg/ffmpeg.exe'
+LOCAL_MUSIC_PATH = MAIN_PATH + '/music/Formatted'
+SETTING_PATH     = MAIN_PATH + '/DB/Music Bot Setting.json'
+LOG_FOLDER_PATH  = MAIN_PATH + '/DB/Music Bot Log Folder'
+EXP_FILE_PATH    = MAIN_PATH + '/DB/Exp Data.json'
+INVALID_URL_PATH = MAIN_PATH + '/DB/invalidUrls.txt'
